ee02260eceaf87dfbae5a8662881cf14fa6998cf
seonjuuu <157812913+seonjuuu@users.noreply.github.com>
2024-02-08 22:39:17 +0900

    Delete Python directory

--- Python/it개론/선형탐색.py
+++
@@ -1,8 +0,0 @@
-def linear(ds, key):
-    for a in range(0, len(ds)):
-        if key == ds[a]:
-            return a
-    return
-
-dataset = [20, 50, 30, 10, 60, 40]
-print(linear(dataset, 10))
--- Python/it개론/하노이탑.py
+++
@@ -1,9 +0,0 @@
-def hanoi(n, A, B, C):
-    if n == 1:
-        print('board', n, 'move', A, '->', C)
-    else:
-        hanoi(n-1, A, C, B)
-        print('board', n, 'move', A, '->', C)
-        hanoi(n-1, B, A, C)
-
-hanoi(4, 'left', 'mid', 'right')
